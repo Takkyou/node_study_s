ecd5ae6ceee30d7a4be04c550a1df3afa58460f1
Kyoungjin Tak <kyoungjintak@Kyoungjinui-MacBookPro.local>
2022-07-11 19:50:05 +0900

    todo list

---
+++ todo/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Todo = require('./models/todo');
+
+mongoose.connect('mongodb://localhost/todo-demo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hi!');
+});
+
+router.get('/todos', async (req, res) => {
+  const todos = await Todo.find().sort('-order').exec();
+
+  res.send({ todos });
+});
+
+router.post('/todos/:todoId', async (req, res) => {
+  const { value } = req.body;
+  const maxOrderTodo = await Todo.findOne().sort('-order').exec();
+  let order = 1;
+
+  if (maxOrderTodo) {
+    order = maxOrderTodo.order + 1;
+  }
+
+  const todo = new Todo({ value, order });
+  await todo.save();
+
+  res.send({ todo });
+});
+
+router.patch('/todos', async (req, res) => {});
+
+app.use('/api', bodyParser.json(), router);
+app.use(express.static('./assets'));
+
+app.listen(8080, () => {
+  console.log('서버가 켜졌어요!');
+});
